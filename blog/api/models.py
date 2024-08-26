8da2178f7bfffc0c9e016a5fd72a9a9a4994caf4
rqc <rqc@rqc.com>
2024-08-26 16:47:42 +0300

    on_delete DO_NOTHING

--- blog/api/models.py
+++ blog/api/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    created_by = models.ForeignKey("auth.User", related_name="post_create_by", on_delete=models.CASCADE)
+    created_by = models.ForeignKey("auth.User", related_name="post_create_by", on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
-    changed_by = models.ForeignKey("auth.User", related_name="post_changed_by", on_delete=models.CASCADE)
+    changed_by = models.ForeignKey("auth.User", related_name="post_changed_by", on_delete=models.DO_NOTHING)
     changed_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -16,9 +16,9 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     content = models.TextField()
-    created_by = models.ForeignKey("auth.User", related_name="comment_create_by", on_delete=models.CASCADE)
+    created_by = models.ForeignKey("auth.User", related_name="comment_create_by", on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
-    changed_by = models.ForeignKey("auth.User", related_name="comment_changed_by", on_delete=models.CASCADE)
+    changed_by = models.ForeignKey("auth.User", related_name="comment_changed_by", on_delete=models.DO_NOTHING)
     changed_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):

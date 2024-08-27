07d532e6840b0d4855d6b60f8e772f05aecb0178
rqc <rqc@rqc.com>
2024-08-27 17:04:35 +0300

    me rework

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -9,26 +9,23 @@ router = DefaultRouter()
 router.register(r"users", views.UserViewSet, basename="user")
 router.register(r"posts", views.PostViewSet, basename="post")
 router.register(r"comments", views.CommentViewSet, basename="comment")
-router.register(r"me/posts", views.PostViewSet, basename="me-post")
-router.register(r"me/comments", views.CommentViewSet, basename="me-comment")
 
-user_nested_router = NestedDefaultRouter(router, r"users", lookup="user")
-user_nested_router.register(r"posts", views.PostViewSet, basename="user-post")
-user_nested_router.register(r"comments", views.CommentViewSet, basename="user-comment")
+user_router = NestedDefaultRouter(router, r"users", lookup="user")
+user_router.register(r"posts", views.PostViewSet, basename="user-post")
+user_router.register(r"comments", views.CommentViewSet, basename="user-comment")
 
-post_nested_router = NestedDefaultRouter(router, r"posts", lookup="post")
-post_nested_router.register(r"comments", views.CommentViewSet, basename="post-comment")
+post_router = NestedDefaultRouter(router, r"posts", lookup="post")
+post_router.register(r"comments", views.CommentViewSet, basename="post-comment")
 
-user_post_nested_router = NestedDefaultRouter(user_nested_router, r"posts", lookup="post")
-user_post_nested_router.register(r"comments", views.CommentViewSet, basename="user-post-comment")
+user_post_router = NestedDefaultRouter(user_router, r"posts", lookup="post")
+user_post_router.register(r"comments", views.CommentViewSet, basename="user-post-comment")
 
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("", include(user_nested_router.urls)),
-    path("", include(post_nested_router.urls)),
-    path("", include(user_post_nested_router.urls)),
-    path("me/", views.UserViewSet.as_view({"get": "retrieve"}), name="me-detail"),
+    path("", include(user_router.urls)),
+    path("", include(post_router.urls)),
+    path("", include(user_post_router.urls)),
     path(
         "me/change_password/",
         lambda request: redirect(request.get_full_path().replace("me", "users"), permanent=True),
--- blog/api/views.py
+++ blog/api/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import NotFound, ValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
@@ -12,6 +12,7 @@ from . import models, permissions, serializers
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    ME_PK = "me"
     queryset = User.objects
     serializer_class = serializers.UserSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
@@ -19,9 +20,8 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     search_fields = ("username", "first_name", "last_name", "email")
     ordering_fields = ("id", "username", "first_name", "last_name", "email", "last_login", "date_joined")
 
-    def retrieve(self, request, pk=None):
-        if pk is not None:
-            return super().retrieve(request, pk)
+    @action(detail=False, methods=["get"], permission_classes=(IsAuthenticated,), url_path=ME_PK)
+    def me(self, request):
         user = request.user
         serializer = self.get_serializer(user)
         return Response(serializer.data)
@@ -29,8 +29,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     @action(
         detail=False,
         methods=["patch"],
-        permission_classes=[IsAuthenticated],
-        name="Change Password",
+        permission_classes=(IsAuthenticated,),
         serializer_class=serializers.ChangePasswordSerializer,
     )
     def change_password(self, request):
@@ -58,15 +57,10 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         result = models.Post.objects
-        if "/me/" in self.request.path:  # TODO
-            result = result.filter(created_by=self.request.user)
-
         user_id = self.kwargs.get("user_pk")
-        if user_id is not None and not User.objects.filter(id=user_id).exists():
-            raise NotFound(f"No {User._meta.object_name} matches the given query.")
         if user_id is not None:
+            user_id = self.request.user.id if user_id == UserViewSet.ME_PK else user_id
             result = result.filter(created_by=user_id)
-
         return result
 
     def perform_create(self, serializer):
@@ -92,23 +86,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         result = models.Comment.objects
-        if "/me/" in self.request.path:  # TODO
-            result = result.filter(created_by=self.request.user)
-
         post_id = self.kwargs.get("post_pk")
-        if post_id is not None and not models.Post.objects.filter(id=post_id).exists():
-            raise NotFound(f"No {models.Post._meta.object_name} matches the given query.")
-
         if post_id is not None:
             result = result.filter(post_id=post_id)
         user_id = self.kwargs.get("user_pk")
-
-        if user_id is not None and not User.objects.filter(id=user_id).exists():
-            raise NotFound(f"No {User._meta.object_name} matches the given query.")
-
         if user_id is not None:
+            user_id = self.request.user.id if user_id == UserViewSet.ME_PK else user_id
             result = result.filter(created_by=user_id)
-
         return result
 
     def perform_create(self, serializer):

e1276a906ef92c14abef69326767b0786ba3727e
rqc <rqc@rqc.com>
2025-03-19 16:29:30 +0300

    .

--- blog/api/admin.py
+++ blog/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import Comment, Post
+from .models import Comment, Post, User
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(User, UserAdmin)
